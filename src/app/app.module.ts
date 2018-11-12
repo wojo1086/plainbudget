8dd08c0a8708d5ba4945781080df2420364be218
Timothy Wojtylak <t.wojtylak@gmail.com>
2018-11-11 22:13:00 -0500

    Adding signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {PbSignupComponent} from './pb-signup/pb-signup.component';
 import {PbHeaderComponent} from './home/pb-header/pb-header.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -23,7 +24,8 @@ import {FlexLayoutModule} from '@angular/flex-layout';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/pb-signup/pb-signup.component.ts
+++ src/app/pb-signup/pb-signup.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'pb-signup',
-  templateUrl: './pb-signup.component.html',
-  styleUrls: ['./pb-signup.component.sass']
+    selector: 'pb-signup',
+    templateUrl: './pb-signup.component.html',
+    styleUrls: ['./pb-signup.component.sass']
 })
 export class PbSignupComponent implements OnInit {
+    signup = {
+        firstName: ''
+    };
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
